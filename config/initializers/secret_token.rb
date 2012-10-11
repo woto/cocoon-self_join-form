b9b9b0bc791f686bbb9b5b0d4b6944ba4e6a9027
Руслан Корнев <oganer@gmail.com>
2012-10-11 14:02:12 +0400

    rails 3.2.8 init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CocoonSelfJoinForm::Application.config.secret_token = '329a445f962263a7b916f7f13ae8b56000f192ecf83633b318ae7c7609e36f221d498f17550356d86229044dac8cfea7222584b1238e1869ce9c9b2df08ef70c'
